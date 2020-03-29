d1d1c78498dc28fa1ae0b77e5edbaa50ce55e9c7
Joseph Hassell <hi@josephhassell.com>
2020-03-29 17:41:49 +0000

    Added Stop music command

--- songs/wnog.lua
+++ songs/wnog.lua
@@ -10,10 +10,15 @@ tc = Timecode:create();
 tc:add(0, function() 
   globals.exec("playsound minecraft:custom.wnog record @a " .. globals.centerpoint.string .. " " .. globals.volume)
 end)
+tc:add(10, function()
+  globals.exec("stopsound @a record")
+end)
 
 tc:add(4, function() 
-  firework.fire(globals.launchzone.barges[0].string, {seconds=2, direction="0.0, 1.0, 0.0"}, "{id:fireworks,Count:1,tag:{Fireworks:{Explosions:[{Type:4,Flicker:1b,Trail:1b,Colors:[I;9437164]},{Type:0}]}}}")
+  firework.fire(globals.launchzone.barges[0].string, {seconds=1, direction="0.0, 1.0, 0.0"}, "{id:fireworks,Count:1,tag:{Fireworks:{Explosions:[{Type:4,Flicker:1b,Trail:1b,Colors:[I;9437164]},{Type:0}]}}}")
 end)
+
+
 
 
 tc:start(function()
